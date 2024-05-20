c8c64101fcecb457a1680b45e60a259dba440b7b
Vincent <101572886+patriknotvincent@users.noreply.github.com>
2024-05-20 11:46:05 +0200

    Update api.tsx

--- src/app/api/api.tsx
+++ src/app/api/api.tsx
@@ -1,4 +1,4 @@
 export async function getDailyHoroscope(sign: String, day: String) {
-    const request = await fetch("http://192.168.56.1:8080/api/horoscope/daily?sign=aquarius")
+    const request = await fetch("http://localhost:8080/api/horoscope/daily?sign=aquarius")
     return await request.json();
 }
