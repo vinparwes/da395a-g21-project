ec6926d33615d13162ed1f39a6d905f445758dd1
Hannah Lindbäck <hannahlindback@gmail.com>
2024-05-20 16:31:25 +0200

    fixed polygon-grid for wheel on startscreen
    - math geniuses, placed buttons on corresponding star sign!
    added some responsive design and background color for startscreen.
    
    Co-Authored-By: simonejonsson <128476989+simonejonsson@users.noreply.github.com>

--- src/app/components/StartScreen/StartScreen.tsx
+++ src/app/components/StartScreen/StartScreen.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import "./StartScreen.css";
 
 function StartScreen() {
+  const signs = [
+    "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", 
+    "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"
+  ];
+
   return (
-    <div>
-      <h1>Welcome to the app!</h1>
-      <img id="birth-chart" src="/birt-chart.png"></img>
+    <div className="pie-chart">
+        <img src="/birth-chart.png" alt="Background Image" className="background-image" />
+        {signs.map((sign, index) => (
+          <div key={index} className={`slice slice-${index + 1}`}>
+            <button className="starsign">{sign}</button>
+          </div>
+        ))}
     </div>
   );
 }
