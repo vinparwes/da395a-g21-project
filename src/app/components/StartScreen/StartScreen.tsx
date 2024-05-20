0fafe0467fa566a8a8e9ccf095f5013252d0caa9
Hannah Lindbäck <hannahlindback@gmail.com>
2024-05-20 13:47:31 +0200

    Fixed StartScreen rendering, img for wheel and tested API req.
    Tested API-request from horoscopeAPI and modified page.tsx to render startscreen first.
    
    Co-Authored-By: simonejonsson <128476989+simonejonsson@users.noreply.github.com>

--- src/app/components/StartScreen/StartScreen.tsx
+++ src/app/components/StartScreen/StartScreen.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./StartScreen.css"
-
+import "./StartScreen.css";
 
 function StartScreen() {
   return (
     <div>
       <h1>Welcome to the app!</h1>
+      <img id="birth-chart" src="/birt-chart.png"></img>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getDailyHoroscope } from './api/api';
+import StartScreen from './components/StartScreen/StartScreen';
 
 export default function Home() {
   const [horoscope, setHoroscope] = useState('');
+  const [start, setStart] = useState(false);
+
   useEffect(() => {
     getDailyHoroscope("foo", "bar").then(response => {
       setHoroscope(response.data.horoscope_data);
     }).catch(error => {
       console.error("Error in fetch", error);
     })
-  }, [])
+  }, []);
 
+  if (!start) {
+    return (
+      <main>
+        <StartScreen />
+        <button onClick={() => setStart(true)}>Start</button>
+      </main>
+    );
+  }
 
   return (
     <main>
