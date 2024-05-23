e51eefa71f81c4d907f32b400db72cbaf99333cf
Vincent <101572886+patriknotvincent@users.noreply.github.com>
2024-05-23 11:47:25 +0200

    Thing works

--- src/app/components/Containers/NavHeader/NavHeader.tsx
+++ src/app/components/Containers/NavHeader/NavHeader.tsx
@@ -8,10 +8,11 @@ import NasaView from "../../Content/NasaView/NasaView";
 import { useState } from "react";
 
 import { useEffect } from "react"
-import { getApod } from "@/app/api/apiNASA"
+import { getApod } from "@/app/api/apiNasa";
+
 
 interface NavHeaderProps {
-  setOpenModal: () => void;
+    setOpenModal: () => void;
 }
 
 export function NavHeader({ setOpenModal }: NavHeaderProps) {
@@ -24,57 +25,59 @@ export function NavHeader({ setOpenModal }: NavHeaderProps) {
         }).catch(error => {
             console.error("Error in fetch", error);
         })
-    }, [nasaData])
+    }, [])
 
-  return (
-    <Navbar
-      fluid
-      rounded
-      className="my-4 position: relative bg-black border-2 border-white"
-    >
-      <Navbar.Brand>
-        <img
-          src="/fav-icon.svg"
-          className="mr-3 h-6 sm:h-11"
-          alt="Flowbite React Logo"
-        />
-        <span className="self-center whitespace-nowrap text-3xl font-semibold text-white">
-          Horos[ Cope ]
-        </span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Button
-          id="new-starsign-btn"
-          onClick={setOpenModal}
-          gradientDuoTone={"purpleToBlue"}
+    return (
+        <Navbar
+            fluid
+            rounded
+            className="my-4 position: relative bg-black border-2 border-white"
         >
-          <span className="mr-1">✩</span>
-          <span>Choose Another Sign</span>
-          <span className="ml-1">☽</span>
-        </Button>
+            <Navbar.Brand>
+                <img
+                    src="/fav-icon.svg"
+                    className="mr-3 h-6 sm:h-11"
+                    alt="Flowbite React Logo"
+                />
+                <span className="self-center whitespace-nowrap text-3xl font-semibold text-white">
+                    Horos[ Cope ]
+                </span>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                <Button
+                    id="new-starsign-btn"
+                    onClick={setOpenModal}
+                    gradientDuoTone={"purpleToBlue"}
+                >
+                    <span className="mr-1">✩</span>
+                    <span>Choose Another Sign</span>
+                    <span className="ml-1">☽</span>
+                </Button>
 
-        <Button
-          id="nasa-fetch"
-          onClick={() => setOpenNasaModal(true)}
-          gradientDuoTone={"purpleToBlue"}
-        >
-          Fetch NASA Data
-        </Button>
-      </Navbar.Collapse>
-      <Modal
-        show={openNasaModal}
-        size="md"
-        popup
-        onClose={() => setOpenNasaModal(false)}
-      >
-        <Modal.Header />
-        <Modal.Body>
-          <NasaView data={nasaData}/>
-        </Modal.Body>
-      </Modal>
-    </Navbar>
-  );
+                <Button
+                    id="nasa-fetch"
+                    onClick={() => setOpenNasaModal(true)}
+                    gradientDuoTone={"purpleToBlue"}
+                >
+                    Fetch NASA Data
+                </Button>
+            </Navbar.Collapse>
+            <Modal
+                show={openNasaModal}
+                size="md"
+                popup
+                onClose={() => setOpenNasaModal(false)}
+            >
+                <Modal.Header className="bg-black" />
+                <Modal.Body
+                    className="bg-black"
+                >
+                    <NasaView nasaData={nasaData} />
+                    </Modal.Body>
+            </Modal>
+        </Navbar>
+    );
 }
 
 export default NavHeader;
--- src/app/components/Content/NasaView/NasaView.tsx
+++ src/app/components/Content/NasaView/NasaView.tsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { Modal } from 'flowbite-react';
 
+interface NasaViewProps {
+    nasaData : string
+}
 
-export default function NasaView({ nasaData }: any) {
-    
-
+export default function NasaView({ nasaData }: NasaViewProps) {
     return (
-        <Modal className="my-4 bg-black object-cover">
+        <>
             <h5 className="text-2xl font-bold tracking-tight text-white dark:text-white">
                 {"NASA Astronomy Picture of the Day"}
             </h5>
-            {nasaData && (
-                <div className="font-normal text-white dark:text-gray-400">
-                    <h2 className="text-xl font-bold">{nasaData.title}</h2>
-                    <p>{nasaData.date}</p>
-                    <img src={nasaData.url} alt={nasaData.title} className="w-full h-auto mt-4 rounded-lg" />
-                    <p className="mt-4">{nasaData.explanation}</p>
-                </div>
-            )}
-        </Modal>
+
+            <div className="font-normal text-white dark:text-gray-400">
+                <h2 className="text-xl font-bold">{nasaData.title}</h2>
+                <p>{nasaData.date}</p>
+                <img src={nasaData.url} alt={nasaData.title} className="w-full h-auto mt-4 rounded-lg" />
+                <p className="mt-4">{nasaData.explanation}</p>
+            </div>
+        </>
     );
 }
